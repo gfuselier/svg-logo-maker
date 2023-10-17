c2c1e2d1fb18c28a91a1e1d6c7fd9f75be3bd750
gfuselier <gisellefuselier@gmail.com>
2023-10-16 20:22:33 -0500

    wrote index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// Includes packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//Creates an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for your logo text:',
+    }, {
+        type: 'input',
+        name: 'text-color',
+        message: 'What color text would you like?',
+    },{
+        type: 'list',
+        name: 'shape',
+        message: 'Which logo shape would you like?',
+        choices: ['circle', 'triangle', 'square']
+    },{
+        type: 'input',
+        name: 'shape-color',
+        message: 'What color shape would you like?'
+    }
+]
+
+inquirer.prompt(questions)
+    .then((data) => {
+        fs.writeFile('./examples/logo.svg', generateLogo(data), (err) => 
+        err ? console.log(err) : console.log('Generated logo.svg'));
+    })
